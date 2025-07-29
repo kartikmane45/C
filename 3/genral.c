5405d4fabdad8d5ad67b30b763f1335549f6ebda
MANE KARTIK <140369878+kartikmane45@users.noreply.github.com>
2025-07-29 22:46:39 +0530

    24/07/25

---
+++ 3/genral.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+//	char ch = 85;
+//	char ch = 125;
+//	char ch = 0x12;
+//	char ch = 0x48;
+//	char ch = 0x2F;
+//	char ch = 11;
+//	char ch = 0x11;
+//	char ch = 011;
+//	char ch = 013;
+//	char ch = 0111;
+//	char ch = 055;
+//	char ch = 0x29;
+//	char ch = 0x101;
+//	char ch = 0101;
+//	char ch = 'a';
+	//char ch = 'e';
+//	char ch = 'O';
+	char ch = 0x1070;
+	printf("%o\n", ch);
+//	printf("%x\n",ch);
+	printf("%d\n",ch);
+//	printf("%c\n", ch);
+
+
+	return 0;
+}
